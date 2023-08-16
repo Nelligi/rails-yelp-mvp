77c87cbdd183fdfa779f5ba060c6e01a13975973
Nelli Igityan <igityan@web.de>
2023-08-16 16:39:23 +0200

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "creating restaurants..."
+
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category:  ["chinese", "italian", "japanese", "french", "belgian"].sample
+    # .sample taking one of the arrays
+  )
+end
+
+puts "creating Reviws"
+5.times do
+  Review.create(
+    rating: rand(0..5),
+    content: Faker::Restaurant.review,
+    restaurant_id: rand(1..5)
+  )
+end
